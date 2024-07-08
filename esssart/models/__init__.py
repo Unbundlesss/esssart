e358df5a181f9fc51d55b465548ed9031d5ef64f
jgusta <jgustafx@gmail.com>
2024-07-08 14:45:22 -0700

    Add initial models and tests structure to 'esssart' package

---
+++ esssart/models/__init__.py
@@ -0,0 +1,7 @@
+from .base import Base
+from .basem2m import BaseM2M
+from .loop import Loop
+from .riff import SharedRiff
+from .attachment import Attachment
+from .joinRiffLoop import JoinRiffLoop
+from .user import User
