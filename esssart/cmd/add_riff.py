013c8041d9fce615c6848132d5a55ff020d92785
jgusta <jgustafx@gmail.com>
2024-07-18 06:04:51 -0700

    outline for riff process

---
+++ esssart/cmd/add_riff.py
@@ -0,0 +1,36 @@
+from ..riffObject import RiffObject
+from .. import db
+
+
+def add_riff(obj: RiffObject):
+    # get image
+    image = obj.image()
+    # make into attachment, get id
+    db.i
+
+    # attach image to riff via id
+
+    # get loops
+
+    # loop through loops and create attachment for each, get id
+
+    # attach audio to loop by adding audio attachment id to loop
+
+    # create loop  in db, collect list of them
+
+    # create riff
+
+    # join riff to loops in db via Join RiffLoop
+
+
+
+
+
+    # get riff
+    riff = obj.riff()
+    # get loops
+    loops = obj.loops()
+
+
+
+    # create riff in db
