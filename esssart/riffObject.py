4b1f294fc7d9a0a93367ea5b6689283d54f54502
jgusta <jgustafx@gmail.com>
2024-07-29 18:29:02 -0700

    start plotting out the add process

--- esssart/riffObject.py
+++ esssart/riffObject.py
@@ -1,8 +1,16 @@
 import json
 import decimal
-import db
+from .db import db
+
 
 class RiffObject:
+
+    _loops=[]
+    _loop_audio=[]
+    _riff=None
+    _image=None
+    _joins=[]
+
     @staticmethod
     def key_to_type(att):
         key = list(att.keys())[0] if att else None
@@ -26,7 +34,7 @@ class RiffObject:
             ),
             dct.get('state', {}).get('playback', [])
         )
-        loop_mains = map(
+        loop_mains = list(map(
             lambda x: dict(
                 id=x.get('_id'),
                 app_version=x.get('app_version'),
@@ -54,9 +62,12 @@ class RiffObject:
                 sample_rate=x.get('sampleRate')
             ),
             dct.get('loops', []),
-        )
+        ))
+
+        loop_ordinals = [{"ordinal": i} for i in list(range(0, loop_mains.__len__()))]
 
         out = dict(
+            image_attachment_id=None,
             id=dct.get('doc_id'),
             action_timestamp=dct.get('action_timestamp'),
             action_timestamp_iso=dct.get('action_timestamp_iso'),
@@ -71,13 +82,15 @@ class RiffObject:
             created=dct.get('rifff', {}).get('created'),
             creators=json.dumps(dct.get('creators', [])),
             database_id=dct.get('database_id'),
-            image_attachment_id=None,
             image=dct.get('image'),
             image_attachment=RiffObject.key_to_type(dct.get('image_attachment')),
             image_url=dct.get('image_url'),
             layer_colors=json.dumps(dct.get('rifff', {}).get('layerColours', [])),
             likes=json.dumps(dct.get('react_counts', {}).get('like', 0)),
-            loops=map(lambda state, main: {**main, **state}, loop_states, loop_mains),
+            loop_ids=[],
+            loops=map(
+                lambda state, main, ordinals: {**main, **state, **ordinals},
+                loop_states, loop_mains, loop_ordinals),
             magnitude=dct.get('rifff', {}).get('magnitude', 0.5),
             peak_data=json.dumps(dct.get('rifff', {}).get('peakData', [])),
             private=dct.get('private', False),
@@ -98,21 +111,68 @@ class RiffObject:
     def __repr__(self):
         return str(self.__dict__)
 
-    def get(self, key):
-        return self.__dict__.get(key)
+    def get(self, key, default=None):
+        return self.__dict__.get(key, default)
 
     def loops(self):
-        return self.__dict__.get('loops')
+        # see if the loops are cached
+        _loops = self.get('_loops', [])
+        if not _loops:
+            # if we dont have the loop ids
+            if not self.get('loop_ids', []):
+                loop_ids = list(map(lambda x: x.id, self.get('loops')))
+                self.set('loop_ids', loop_ids)
+                loops = list(map(lambda x: db.loop.create_loop(x), self.get('loops')))
+                return loops
+            else:
+                return list(map(lambda x: db.loop.get_loop(x), self.get('loop_ids')))
+
+    def process_loops(self):
+        for loop in self.loops():
+            self.parse_audio(loop)
+
+    def _loopy(self, l):
+        audio = self._audio(l)
+        last = db.loop.create_loop(l)
+        l.set
+        return last
+
+    def parse_audio(self, loop):
+        """create the audio attach ment for a loop if not exists.
+        Return the audio attachment"""
+
+        # if the audio is cached, return it
+        if self._loop_audio[loop.ordinal]:
+            return self._loop_audio[loop.ordinal]
+
+        # if the attachment_id is not set, then the audio is made
+        if loop.get('audio_attachment_id') is None:
+            audio = db.attachment.create_attachment(loop.audio_attachment)
+            self._loop_audio[loop.ordinal] = audio
+            loop.set('audio_attachment_id', audio.id)
+
+        # get the audio attachment
+        audio = db.attachment.get_attachment(loop.get('audio_attachment_id'))
+        return audio
 
     def riff(self):
-       return {i: self.__dict__.get(i) for i in db.shared_riff.get_fields()}
+        fields = {i: self.__dict__.get(i) for i in db.shared_riff.get_fields()}
+        riff = db.shared_riff.create_shared_riff(fields)
+
+
+        # join loops using join_riff_loop
+        db.join_riff_loop.join_loops_to_riff(riff, self.loops())
 
     def image(self):
-        return self.__dict__.get('image_attachment')
+        if self.get("image_attachment_id") is None:
+            image = db.attachment.create_attachment(self.get('image_attachment'))
+            self.set('image_attachment_id', image.id)
+        else:
+            image = db.attachment.get_attachment(self.get('image_attachment_id'))
+        return image
 
     def remove(self, key):
         del self.__dict__[key]
 
     def set(self, key, value):
         self.__dict__[key] = value
-
